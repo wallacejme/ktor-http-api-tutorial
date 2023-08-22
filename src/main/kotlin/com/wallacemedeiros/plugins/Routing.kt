49fc5e06a06eb55a291cf7a5e628b6d5adab52df
Wallace Medeiros <wallacejme@gmail.com>
2023-08-21 21:04:27 -0300

    Remove rota "/"

--- src/main/kotlin/com/wallacemedeiros/plugins/Routing.kt
+++ src/main/kotlin/com/wallacemedeiros/plugins/Routing.kt
@@ -2,14 +2,10 @@ package com.wallacemedeiros.plugins
 
 import com.wallacemedeiros.routes.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
         customerRouting()
         listOrderRouting()
         getOrderRoute()
