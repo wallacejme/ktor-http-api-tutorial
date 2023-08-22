fdd9e4f5b3e69e27e0992f77d7bc09790e079800
Wallace Medeiros <wallacejme@gmail.com>
2023-08-21 22:50:19 -0300

    Cria alguns testes automatizados

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ application {
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    kotlin {
+        jvmToolchain(11)
+    }
 }
 
 repositories {
@@ -25,6 +28,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- src/test/kotlin/com/wallacemedeiros/ApplicationTest.kt
+++ src/test/kotlin/com/wallacemedeiros/ApplicationTest.kt
@@ -1,1 +1,83 @@
 package com.wallacemedeiros
+
+import com.wallacemedeiros.models.Customer
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class CustomerRouteTest {
+    private val luizGonzaga = Customer("1", "Luiz", "Gonzaga", "luiz.gonzaga@musica.com")
+    @Test
+    fun testCreateCustomer() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        val response = client.post("/customer") {
+            contentType(ContentType.Application.Json)
+            setBody(luizGonzaga)
+        }
+        assertEquals("Cliente criado com sucesso", response.bodyAsText())
+        assertEquals(HttpStatusCode.Created, response.status)
+    }
+
+    @Test
+    fun testGetCustomer() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        val response = client.get("/customer/${luizGonzaga.id}")
+        assertEquals(luizGonzaga, response.body())
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+
+    @Test
+    fun testGetNonExistentCustomer() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        val id = "2"
+        val response = client.get("/customer/$id")
+        assertEquals("Nenhum cliente com id $id", response.bodyAsText())
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+}
+
+class OrderRouteTests {
+    @Test
+    fun testGetOrder() = testApplication {
+        val response = client.get("/order/2020-04-06-01")
+        assertEquals(
+            """{"number":"2020-04-06-01","contents":[{"item":"Ham Sandwich","amount":2,"price":5.5},{"item":"Water","amount":1,"price":1.5},{"item":"Beer","amount":3,"price":2.3},{"item":"Cheesecake","amount":1,"price":3.75}]}""",
+            response.bodyAsText()
+        )
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+
+    @Test
+    fun testGetNonExistentOrder() = testApplication {
+        val response = client.get("/order/2020-04-06-02")
+        assertEquals(
+            "Pedido não encontrado",
+            response.bodyAsText()
+        )
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+
+    @Test
+    fun testTotatlizeOrder() = testApplication {
+        val response = client.get("/order/2020-04-06-01/total")
+        assertEquals(23.15, response.bodyAsText().toDouble())
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+}
