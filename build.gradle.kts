577cd4fef7861f7d6a22173808eeb5d7cb00d330
Wallace Medeiros <wallacejme@gmail.com>
2023-09-03 10:55:08 -0300

    Usa java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ application {
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
     kotlin {
-        jvmToolchain(11)
+        jvmToolchain(17)
     }
 }
 
